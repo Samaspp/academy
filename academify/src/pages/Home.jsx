172ea42a0550b954a0b36c2447b12d848a13efc5
sami <samiap914@gmail.com>
2024-03-06 12:55:34 +0530

    home page

--- academify/src/pages/Home.jsx
+++ academify/src/pages/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/home.css'
 
 function Home() {
   return (
-    <div>
+    <div className='home'>
+      <div className='container'>
+        <div className='student'>
+          <h1>I am a student</h1>
+        </div>
+        <div className='teacher'>
+          <h1>I am a teacher</h1>
+        </div>
+      </div>
         
     </div>
   )
